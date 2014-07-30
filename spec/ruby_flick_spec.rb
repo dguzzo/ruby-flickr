a343954b6ba94158caf220c27dee68824b86cb87
Dominick Guzzo <dguzzo@gmail.com>
2014-07-29 19:03:32 -0700

    convert to new 'expect' syntax
    recommended, see:
    http://myronmars.to/n/dev-blog/2012/06/rspecs-new-expectation-syntax

--- spec/ruby_flick_spec.rb
+++ spec/ruby_flick_spec.rb
@@ -15,30 +15,31 @@ describe "ruby_flickr" do
     it "sets FlickRaw.api_key and FlickRaw.shared_secret" do
       Settings::stub(:authentication).and_return({:api_key => 'fake-api-key', :shared_secret => "fake-shared-secret"})
       flickraw_basic = RubyFlickr::API.new
-      FlickRaw.api_key.should eq('fake-api-key')
-      FlickRaw.shared_secret.should eq('fake-shared-secret')
+      expect(FlickRaw.api_key).to eq('fake-api-key')
+      expect(FlickRaw.shared_secret).to eq('fake-shared-secret')
     end
 
     it "doesn't set FlickRaw.shared_secret if not present in Settings" do
       Settings::stub(:authentication).and_return({:api_key => 'fake-api-key'})
       flickraw_basic = RubyFlickr::API.new
-      FlickRaw.api_key.should eq('fake-api-key')
-      FlickRaw.shared_secret.should be_nil
+      expect(FlickRaw.api_key).to eq('fake-api-key')
+      expect(FlickRaw.shared_secret).to be_nil
     end
 
     it "doesn't set FlickRaw.api_key if not present in Settings" do
       Settings::stub(:authentication).and_return({:shared_secret => 'fake-shared-secret'})
       flickraw_basic = RubyFlickr::API.new
-      FlickRaw.shared_secret.should eq('fake-shared-secret')
-      FlickRaw.api_key.should be_nil
+      expect(FlickRaw.shared_secret).to eq('fake-shared-secret')
+      expect(FlickRaw.api_key).to be_nil
     end
   end
 
   describe "load_settings" do
     it "should call Settings.load!" do
       Settings::stub(:authentication).and_return({:api_key => 'fake-api-key', :shared_secret => "fake-shared-secret"})
-      Settings.should_receive(:load!)
+      Settings::stub(:load!)
       flickraw_basic = RubyFlickr::API.new
+      expect(Settings).to have_received(:load!)
     end
   end
 
@@ -50,21 +51,21 @@ describe "ruby_flickr" do
 
     it "sanitizes properly" do
       #testing a private method
-      flickraw_basic.send(:sanitize_filename, "something with spaces").should eq("something_with_spaces")
-      flickraw_basic.send(:sanitize_filename, "something_with_underscores").should eq("something_with_underscores")
-      flickraw_basic.send(:sanitize_filename, "something-with-dashes").should eq("something-with-dashes")
-      flickraw_basic.send(:sanitize_filename, "something with an extension.jpg").should eq("something_with_an_extension.jpg")
-      flickraw_basic.send(:sanitize_filename, "something/with/slashes").should eq("something_with_slashes")
-      flickraw_basic.send(:sanitize_filename, 'something/with//slashes.and.others').should eq('something_with__slashes.and.others')
-      flickraw_basic.send(:sanitize_filename, "something@with!stuff").should eq("something_with_stuff")
+      expect(flickraw_basic.send(:sanitize_filename, "something with spaces")).to eq("something_with_spaces")
+      expect(flickraw_basic.send(:sanitize_filename, "something_with_underscores")).to eq("something_with_underscores")
+      expect(flickraw_basic.send(:sanitize_filename, "something-with-dashes")).to eq("something-with-dashes")
+      expect(flickraw_basic.send(:sanitize_filename, "something with an extension.jpg")).to eq("something_with_an_extension.jpg")
+      expect(flickraw_basic.send(:sanitize_filename, "something/with/slashes")).to eq("something_with_slashes")
+      expect(flickraw_basic.send(:sanitize_filename, 'something/with//slashes.and.others')).to eq('something_with__slashes.and.others')
+      expect(flickraw_basic.send(:sanitize_filename, "something@with!stuff")).to eq("something_with_stuff")
     end
 
     it "returns a canned title for a bad filename" do
-      flickraw_basic.send(:sanitize_filename, nil).should eq("bad-file-name")
+      expect(flickraw_basic.send(:sanitize_filename, nil)).to eq("bad-file-name")
     end
 
     it "returns 'untitled' for a missing filename" do
-      flickraw_basic.send(:sanitize_filename, "").should eq("")
+      expect(flickraw_basic.send(:sanitize_filename, "")).to eq("")
     end
 
     it "should throw if no filename is passed" do
